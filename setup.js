b5749c3cf62e408c98966c2cc991e3f077e00b2a
Randyyyyy. <rynnkunnnn@gmail.com>
2026-05-28 23:23:38 +0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 fs.mkdirSync(path.join(__dirname, 'models'), { recursive: true });
 
 execSync(
-    'pip install huggingface_hub && python -c "' +
+    'pip install huggingface_hub && python3 -c "' +
     'from huggingface_hub import hf_hub_download;' +
     'hf_hub_download(\'SmilingWolf/wd-swinv2-tagger-v3\', \'model.onnx\', local_dir=\'models\');' +
     'hf_hub_download(\'SmilingWolf/wd-swinv2-tagger-v3\', \'selected_tags.csv\', local_dir=\'models\')' +
